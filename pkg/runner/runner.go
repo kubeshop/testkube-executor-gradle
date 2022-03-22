02372c5bc188eb71b62cd4a981db73f55cb5c5d5
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2022-03-22 12:30:01 +0100

    Added Gradle runner implementation and tests.

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -1,50 +1,133 @@
 package runner
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	junit "github.com/joshdk/go-junit"
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
-	"github.com/kubeshop/testkube/pkg/executor/content"
+	"github.com/kubeshop/testkube/pkg/executor"
 	"github.com/kubeshop/testkube/pkg/executor/output"
 )
 
-func NewRunner() *ExampleRunner {
-	return &ExampleRunner{
-		Fetcher: content.NewFetcher(""),
+type Params struct {
+	Datadir string // RUNNER_DATADIR
+}
+
+func NewRunner() *GradleRunner {
+	params := Params{
+		Datadir: os.Getenv("RUNNER_DATADIR"),
 	}
+
+	runner := &GradleRunner{
+		params: params,
+	}
+
+	return runner
 }
 
-// ExampleRunner for template - change me to some valid runner
-type ExampleRunner struct {
-	Fetcher content.ContentFetcher
+type GradleRunner struct {
+	params Params
 }
 
-func (r *ExampleRunner) Run(execution testkube.Execution) (result testkube.ExecutionResult, err error) {
-	path, err := r.Fetcher.Fetch(execution.Content)
-	if err != nil {
+func (r *GradleRunner) Run(execution testkube.Execution) (result testkube.ExecutionResult, err error) {
+	// check that the datadir exists
+	_, err = os.Stat(r.params.Datadir)
+	if errors.Is(err, os.ErrNotExist) {
 		return result, err
 	}
 
-	output.PrintEvent("created content path", path)
-
+	// the Gradle executor does not support files
 	if execution.Content.IsFile() {
-		output.PrintEvent("using file", execution)
-		// TODO implement file based test content for string, git-file, file-uri
-		//      or remove if not used
+		return result.Err(fmt.Errorf("executor only support git-dir based tests")), nil
 	}
 
-	if execution.Content.IsDir() {
-		output.PrintEvent("using dir", execution)
-		// TODO implement file based test content for git-dir
-		//      or remove if not used
+	// check settings.gradle or settings.gradle.kts files exist
+	directory := filepath.Join(r.params.Datadir, "repo")
+	settingsGradle := filepath.Join(directory, "settings.gradle")
+	settingsGradleKts := filepath.Join(directory, "settings.gradle.kts")
+
+	_, settingsGradleErr := os.Stat(settingsGradle)
+	_, settingsGradleKtsErr := os.Stat(settingsGradleKts)
+	if errors.Is(settingsGradleErr, os.ErrNotExist) && errors.Is(settingsGradleKtsErr, os.ErrNotExist) {
+		return result.Err(fmt.Errorf("no settings.gradle or settings.gradle.kts found")), nil
 	}
 
-	// TODO run executor here
+	// determine the Gradle command to use
+	gradleCommand := "gradle"
+	gradleWrapper := filepath.Join(directory, "gradlew")
+	_, err = os.Stat(gradleWrapper)
+	if err == nil {
+		// then we use the wrapper instead
+		gradleCommand = "./gradlew"
+	}
 
-	// error result should be returned if something is not ok
-	// return result.Err(fmt.Errorf("some test execution related error occured"))
+	// simply set the ENVs to use during Gradle execution
+	for key, value := range execution.Envs {
+		os.Setenv(key, value)
+	}
 
-	// TODO return ExecutionResult
-	return testkube.ExecutionResult{
-		Status: testkube.StatusPtr(testkube.SUCCESS_ExecutionStatus),
-		Output: "exmaple test output",
-	}, nil
+	// pass additional executor arguments/flags to Gradle
+	args := []string{"--no-daemon"}
+	args = append(args, execution.Args...)
+
+	if !strings.EqualFold(execution.TestType, "gradle/project") {
+		// then use the test subtype as task name
+		task := strings.Split(execution.TestType, "/")[1]
+		args = append(args, task)
+	}
+
+	output.PrintEvent("Running", directory, gradleCommand, args)
+	output, err := executor.Run(directory, gradleCommand, args...)
+
+	if err == nil {
+		result.Status = testkube.ExecutionStatusSuccess
+	} else {
+		result.Status = testkube.ExecutionStatusError
+		result.ErrorMessage = err.Error()
+		if strings.Contains(result.ErrorMessage, "exit status 1") {
+			// probably some tests have failed
+			result.ErrorMessage = "build failed with an exception"
+		} else {
+			// Gradle was unable to run at all
+			return result, nil
+		}
+	}
+
+	result.Output = string(output)
+	result.OutputType = "text/plain"
+
+	junitReportPath := filepath.Join(directory, "build", "test-results", args[len(args)-1])
+	err = filepath.Walk(junitReportPath, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && filepath.Ext(path) == ".xml" {
+			suites, _ := junit.IngestFile(path)
+			for _, suite := range suites {
+				for _, test := range suite.Tests {
+					result.Steps = append(
+						result.Steps,
+						testkube.ExecutionStepResult{
+							Name:     fmt.Sprintf("%s - %s", suite.Name, test.Name),
+							Duration: test.Duration.String(),
+							Status:   mapStatus(test.Status),
+						})
+				}
+			}
+		}
+
+		return nil
+	})
+
+	return result, err
+}
+
+func mapStatus(in junit.Status) (out string) {
+	switch string(in) {
+	case "passed":
+		return string(testkube.SUCCESS_ExecutionStatus)
+	default:
+		return string(testkube.ERROR__ExecutionStatus)
+	}
 }
--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -1,19 +1,38 @@
 package runner
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
+
+	cp "github.com/otiai10/copy"
 
 	"github.com/kubeshop/testkube/pkg/api/v1/testkube"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRun(t *testing.T) {
+func TestRunGradle(t *testing.T) {
 
-	t.Run("runner should run test based on execution data", func(t *testing.T) {
+	t.Run("run gradle wrapper project with explicit target arg", func(t *testing.T) {
+		// setup
+		tempDir, _ := os.MkdirTemp("", "*")
+		os.Setenv("RUNNER_DATADIR", tempDir)
+		repoDir := filepath.Join(tempDir, "repo")
+		os.Mkdir(repoDir, 0755)
+		_ = cp.Copy("../../examples/hello-gradlew", repoDir)
+
 		// given
 		runner := NewRunner()
 		execution := testkube.NewQueuedExecution()
-		execution.Content = testkube.NewStringTestContent("hello I'm test content")
+		execution.TestType = "gradle/project"
+		execution.Content = &testkube.TestContent{
+			Type_: string(testkube.TestContentTypeGitDir),
+			Repository: &testkube.Repository{
+				Uri:    "https://github.com/lreimer/hands-on-testkube.git",
+				Branch: "main",
+			},
+		}
+		execution.Args = []string{"test"}
 
 		// when
 		result, err := runner.Run(*execution)
@@ -21,6 +40,100 @@ func TestRun(t *testing.T) {
 		// then
 		assert.NoError(t, err)
 		assert.Equal(t, result.Status, testkube.ExecutionStatusSuccess)
+		assert.Len(t, result.Steps, 1)
 	})
 
+	t.Run("run gradle project test with envs", func(t *testing.T) {
+		// setup
+		tempDir, _ := os.MkdirTemp("", "*")
+		os.Setenv("RUNNER_DATADIR", tempDir)
+		repoDir := filepath.Join(tempDir, "repo")
+		os.Mkdir(repoDir, 0755)
+		_ = cp.Copy("../../examples/hello-gradle", repoDir)
+
+		// given
+		runner := NewRunner()
+		execution := testkube.NewQueuedExecution()
+		execution.TestType = "gradle/test"
+		execution.Content = &testkube.TestContent{
+			Type_: string(testkube.TestContentTypeGitDir),
+			Repository: &testkube.Repository{
+				Uri:    "https://github.com/lreimer/hands-on-testkube.git",
+				Branch: "main",
+			},
+		}
+		execution.Envs = map[string]string{"TESTKUBE_GRADLE": "true"}
+
+		// when
+		result, err := runner.Run(*execution)
+
+		// then
+		assert.NoError(t, err)
+		assert.Equal(t, result.Status, testkube.ExecutionStatusSuccess)
+		assert.Len(t, result.Steps, 1)
+	})
+}
+
+func TestRunErrors(t *testing.T) {
+
+	t.Run("no RUNNER_DATADIR", func(t *testing.T) {
+		os.Setenv("RUNNER_DATADIR", "/unknown")
+
+		// given
+		runner := NewRunner()
+		execution := testkube.NewQueuedExecution()
+
+		// when
+		_, err := runner.Run(*execution)
+
+		// then
+		assert.Error(t, err)
+	})
+
+	t.Run("unsupported file-content", func(t *testing.T) {
+		tempDir := os.TempDir()
+		os.Setenv("RUNNER_DATADIR", tempDir)
+
+		// given
+		runner := NewRunner()
+		execution := testkube.NewQueuedExecution()
+		execution.TestType = "gradle/project"
+		execution.Content = testkube.NewStringTestContent("")
+
+		// when
+		result, err := runner.Run(*execution)
+
+		// then
+		assert.NoError(t, err)
+		assert.Equal(t, result.Status, testkube.ExecutionStatusError)
+	})
+
+	t.Run("no settings.gradle", func(t *testing.T) {
+		// setup
+		tempDir, _ := os.MkdirTemp("", "*")
+		os.Setenv("RUNNER_DATADIR", tempDir)
+
+		repoDir := filepath.Join(tempDir, "repo")
+		os.Mkdir(repoDir, 0755)
+
+		// given
+		runner := NewRunner()
+		execution := testkube.NewQueuedExecution()
+		execution.TestType = "gradle/project"
+		execution.Content = &testkube.TestContent{
+			Type_: string(testkube.TestContentTypeGitDir),
+			Repository: &testkube.Repository{
+				Uri:    "https://github.com/lreimer/hands-on-testkube.git",
+				Branch: "main",
+			},
+		}
+
+		// when
+		result, err := runner.Run(*execution)
+
+		// then
+		assert.NoError(t, err)
+		assert.Equal(t, result.Status, testkube.ExecutionStatusError)
+		assert.Contains(t, result.ErrorMessage, "no")
+	})
 }
